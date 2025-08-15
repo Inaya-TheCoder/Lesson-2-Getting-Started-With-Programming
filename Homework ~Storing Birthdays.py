cd96f8c8992557904121973b224d9483e6d69abf
Inaya-TheCoder <inaya.singh2013@gmail.com>
2025-08-15 22:36:51 +0530

    Homework - Birthdates of my Fmily Members

---
+++ Homework ~Storing Birthdays.py
@@ -0,0 +1,10 @@
+print ("Birthdates Of My Family")
+Dad = "25/8,"
+Mom = "29/9,"
+Inaya = "3/1,"
+Cooper= "6/5,"
+Grandfather = "1/1,"
+Grandmother = "1/7," 
+MaternalGrandfather = "17/7,"
+MaternalGrandmother = "31/12"
+print (Dad,Mom,Inaya,Cooper,Grandfather,Grandmother,MaternalGrandfather,MaternalGrandmother ,"are the birth dates of my family members")
